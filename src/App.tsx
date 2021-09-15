ea55dd59f8518c0737b6835af0ca0c973ba46123
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-15 16:35:24 +0200

    WIP Set up start quiz

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ function App() {
   return (
     <div className="App">
       <h1>The Impossible Quiz</h1>
+        <button className="start" onClick={startQuiz}>Start Quiz</button>
+        <p className="score">Score:</p>
+        <p>Questions loading...</p>
       <QuestionCard/>
     </div>
 
