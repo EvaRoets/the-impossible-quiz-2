15aae5a9194fdec99f045778573c50df1feffec3
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-15 16:41:51 +0200

    Pass required props in question card

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     const [loading, setLoading] = useState(false);
     const [questions, setQuestions] = useState([]);
     const [number, setNumber] = useState(0);
-    const [playerAnsers, setPLayerAnswers] = useState([]);
+    const [playerAnswers, setPLayerAnswers] = useState([]);
     const [score, setScore] = useState(0);
     const [gameOver, setGameOver] = useState(true);
 
@@ -18,7 +18,14 @@ const App = () => {
         <button className="start" onClick={startQuiz}>Start Quiz</button>
         <p className="score">Score:</p>
         <p>Questions loading...</p>
-      <QuestionCard/>
+      <QuestionCard
+          questionNum={number + 1}
+          totalQuestions={10}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          playerAnswer={playerAnswers ? playerAnswers[number] : undefined}
+          callback={checkAnswer}
+      />
         <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
