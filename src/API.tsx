75159967ad3e72b5d2445f3ac7af7fb79da83abc
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-16 11:35:12 +0200

    Define question type

--- src/API.tsx
+++ src/API.tsx
@@ -1,8 +1,21 @@
+export type Question = {
+    category: string;
+    correct_answer: string;
+    difficulty: string;
+    incorrect_answer: string[];
+    question: string;
+    type: string;
+}
+
+
+
+
+
 // Add async to return promises
 // Add amount as parameter, but all the other options could be adjusted too
 
 export const fetchQuizQuestions = async (amount: number) => {
-    const endPoint = `https://opentdb.com/api.php?amount=${amount}&category=17&difficulty=medium&type=multiple`;
+    const endPoint = `https://opentdb.com/api.php?amount=${amount}&category=17&difficulty=easy&type=multiple`;
     // await fetch, then await conversion to json
     const data = await (await fetch(endPoint)).json();
     console.log(data)
