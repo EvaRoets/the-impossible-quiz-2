2aed893298c495e8d975b0f5c0449a04864690e7
Eva Roets <84382812+EvaRoets@users.noreply.github.com>
2021-09-16 11:47:44 +0200

    Return all answers (correct + incorrect)

--- src/API.tsx
+++ src/API.tsx
@@ -1,3 +1,4 @@
+// create own type
 export type Question = {
     category: string;
     correct_answer: string;
@@ -11,13 +12,18 @@ export type Question = {
 export type QuestionState = Question & { answers: string []};
 
 
-
 // Add async to return promises
 // Add amount as parameter, but all the other options could be adjusted too
-
 export const fetchQuizQuestions = async (amount: number) => {
     const endPoint = `https://opentdb.com/api.php?amount=${amount}&category=17&difficulty=easy&type=multiple`;
     // await fetch, then await conversion to json
     const data = await (await fetch(endPoint)).json();
-    console.log(data)
+    return data.results.map((question: Question) => (
+        //return object
+        {
+            //grab all properties of the question with spread operator
+            ...question,
+            answer: [...question.incorrect_answer, question.correct_answer]
+        }
+    ))
 }
